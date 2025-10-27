003d0d7db19fbd029b4b70d6b9e6daf71d510efb
remi <remi.petit93370@gmail.com>
2025-10-27 14:22:14 +0100

    supprimer une fonction inutile main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from fastapi import FastAPI
-import os
-import uvicorn
 from app.routes import health, metro
 from app.middleware.logging import LoggingMiddleware
 
@@ -12,9 +10,3 @@ app.add_middleware(LoggingMiddleware)
 # Inclure les routes
 app.include_router(health.router)
 app.include_router(metro.router)
-
-if __name__ == "__main__":
-    PORT = int(os.getenv("PORT", 3000))
-    print(f"🚇 Last Metro API démarrée sur http://localhost:{PORT}")
-    print(f"📊 Health check: http://localhost:{PORT}/health")
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
