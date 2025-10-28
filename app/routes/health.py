811483cb3a46fd5ffe5d03829fd8a5de09f8db15
remi <remi.petit93370@gmail.com>
2025-10-28 09:40:45 +0100

    init

--- app/routes/health.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.controllers.health import health_check
-
-router = APIRouter()
-
-@router.get("/health")
-async def get_health_check():
-    return await health_check()
